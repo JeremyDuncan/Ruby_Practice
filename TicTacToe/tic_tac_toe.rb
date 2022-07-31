604f3a835f580cc2f81eb3bc809625f28a15f460
Jeremy Duncan <jduncan46@my.devry.edu>
2022-07-31 16:37:06 -0400

    created player class

---
+++ TicTacToe/tic_tac_toe.rb
@@ -0,0 +1,8 @@
+# Build a tic-tac-toe game on the command line where two human players can play against each other and the board is displayed in between turns.
+
+class Player
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+end
